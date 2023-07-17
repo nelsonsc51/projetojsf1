46ffb6baf5a86f1e0a64b145ae2d83ce6adcc804
Nelson Costa <nelsonscosta@yahoo.com.br>
2023-07-17 12:45:53 -0300

    criado campos para foto upload

--- projetojsf/src/main/java/br/com/entidades/Pessoa.java
+++ projetojsf/src/main/java/br/com/entidades/Pessoa.java
@@ -4,10 +4,14 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -66,6 +70,14 @@ public class Pessoa implements Serializable {
 	@ManyToOne
 	private Cidades cidades;
 	
+	@Column(columnDefinition = "text") //Tipo text para arquivos base 64
+	private String fotoIconBase64;
+	
+	private String extensao; 
+	
+	@Lob
+	@Basic(fetch = FetchType.LAZY)
+	private byte[] fotoIconBase64Original;
 		
 	@Temporal(TemporalType.DATE)
 	private Date dataNascimento = new Date()	;
@@ -248,8 +260,32 @@ public class Pessoa implements Serializable {
 	public Cidades getCidades() {
 		return cidades;
 	}
-		
 	
+	
+	public String getFotoIconBase64() {
+		return fotoIconBase64;
+	}
+
+	public void setFotoIconBase64(String fotoIconBase64) {
+		this.fotoIconBase64 = fotoIconBase64;
+	}
+
+	public String getExtensao() {
+		return extensao;
+	}
+
+	public void setExtensao(String extensao) {
+		this.extensao = extensao;
+	}
+
+	public byte[] getFotoIconBase64Original() {
+		return fotoIconBase64Original;
+	}
+
+	public void setFotoIconBase64Original(byte[] fotoIconBase64Original) {
+		this.fotoIconBase64Original = fotoIconBase64Original;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
